e65313aeca4de960362d1cf6561bea2e6e051d41
Connor Simmons <coshnor@gmail.com>
2024-04-19 13:10:11 -0400

    started working on project

---
+++ README.md
@@ -0,0 +1,7 @@
+<p> 
+testing
+</p>
+
+<p>
+second paragraph
+</p>
--- src/Routing.java
+++ src/Routing.java
@@ -11,6 +11,36 @@ public class Routing {
      */
     public static ArrayList<Wire>
     findPaths(Board board, ArrayList<Endpoints> goals) {
+
+        //Might need another auxiliary data structure to keep track of each list of coords
+        //at a time before each "wire" is finalized then added to this list
+        ArrayList<Wire> paths = new ArrayList<>();
+
+
+
+        /*
+        * BFS Loop:
+        */
+
+        for(Endpoints endpoint : goals){
+
+        }
+
+
+        /*
+        General outline of everything(initial idea):
+        Create: Array of Lists? Maybe a 2d array instead? where each -row? column?-
+                one of the two is storing, in order the coords of each node in the path
+        for each starting point in "goals":
+            do bfs on the grid, searching for the "endpoint", storing each node on the path
+            in the correct array/list
+
+
+        NOTE TO SELF:
+            eventually will want to figure out how to "experiment with improvements to BFS"
+            or "alternative algorithms"
+        */
+
         return null;  // replace this line with your code
     }
 
